02b0c3cf624dad26817a6297bfcdeb7669641f9f
evyatarmeged <evyatar.m@otterize.com>
2022-07-11 15:58:49 +0300

    Intents validator adjustment

--- src/operator/controllers/reconcilers/intents_validator.go
+++ src/operator/controllers/reconcilers/intents_validator.go
@@ -2,8 +2,9 @@ package reconcilers
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	otterizev1alpha1 "github.com/otterize/otternose/api/v1alpha1"
+	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -20,12 +21,31 @@ func (r *IntentsValidatorReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	if err != nil {
 		return ctrl.Result{}, err
 	}
+
 	for _, service := range intents.Spec.Services {
-		fmt.Println("Intents for service: " + service.Name)
+		logrus.Debugln("Intents for service: " + service.Name)
 		for _, intent := range service.Calls {
-			fmt.Printf("%s has intent to access %s. Intent type: %s\n", service.Name, intent.Server, intent.Type)
+			logrus.Debugf("%s has intent to access %s. Intent type: %s\n", service.Name, intent.Server, intent.Type)
+			if err := validateIntent(intent); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	}
+	return ctrl.Result{}, nil
+}
+
+func validateIntent(intent otterizev1alpha1.Intent) error {
+	if intent.Type == otterizev1alpha1.IntentTypeKafka {
+		if intent.HTTPResources != nil {
+			return errors.New("invalid intent format. type 'Kafka' cannot contain HTTP resources")
 		}
 	}
 
-	return ctrl.Result{}, nil
+	if intent.Type == otterizev1alpha1.IntentTypeHTTP {
+		if intent.Topics != nil {
+			return errors.New("invalid intent format. type 'HTTP' cannot contain kafka topics")
+		}
+	}
+
+	return nil
 }
