457e059effac5c6489e25db563e5002403b78f60
Evyatar Meged <evyatarmeged@gmail.com>
2023-04-17 19:23:15 +0300

    Add HTTP methods to istio watcher (#95)

--- src/exp/istio-watcher/pkg/watcher/helpers.go
+++ src/exp/istio-watcher/pkg/watcher/helpers.go
@@ -1,35 +1,71 @@
 package istiowatcher
 
 import (
-	"fmt"
 	"github.com/otterize/network-mapper/src/exp/istio-watcher/mapperclient"
 	"github.com/samber/lo"
+	"net/http"
 	"time"
 )
 
+var HTTPMethodsToGQLMethods = map[string]mapperclient.HttpMethod{
+	http.MethodGet:     mapperclient.HttpMethodGet,
+	http.MethodPost:    mapperclient.HttpMethodPost,
+	http.MethodPut:     mapperclient.HttpMethodPut,
+	http.MethodDelete:  mapperclient.HttpMethodDelete,
+	http.MethodOptions: mapperclient.HttpMethodOptions,
+	http.MethodTrace:   mapperclient.HttpMethodTrace,
+	http.MethodPatch:   mapperclient.HttpMethodPatch,
+	http.MethodConnect: mapperclient.HttpMethodConnect,
+}
+
+type ConnectionPairWithPath struct {
+	SourceWorkload      string `json:"sourceWorkload"`
+	DestinationWorkload string `json:"destinationWorkload"`
+	RequestPath         string `json:"requestPath"`
+}
+
 func ToGraphQLIstioConnections(connections map[ConnectionWithPath]time.Time) []mapperclient.IstioConnection {
-	connectionPairToConn := map[string]mapperclient.IstioConnection{}
+	connectionPairToGraphQLConnection := map[ConnectionPairWithPath]mapperclient.IstioConnection{}
+
 	for connWithPath, timestamp := range connections {
-		connectionPair := fmt.Sprintf("%s.%s", connWithPath.SourceWorkload, connWithPath.DestinationWorkload)
-		istioConnection, ok := connectionPairToConn[connectionPair]
+		connectionPair := ConnectionPairWithPath{
+			SourceWorkload:      connWithPath.SourceWorkload,
+			DestinationWorkload: connWithPath.DestinationWorkload,
+			RequestPath:         connWithPath.RequestPath,
+		}
+
+		istioConnection, ok := connectionPairToGraphQLConnection[connectionPair]
 		if !ok {
-			connectionPairToConn[connectionPair] = mapperclient.IstioConnection{
+			istioConnection = mapperclient.IstioConnection{
 				SrcWorkload:          connWithPath.SourceWorkload,
 				SrcWorkloadNamespace: connWithPath.SourceNamespace,
 				DstWorkload:          connWithPath.DestinationWorkload,
 				DstWorkloadNamespace: connWithPath.DestinationNamespace,
 				Path:                 connWithPath.RequestPath,
-				Methods:              []mapperclient.HttpMethod{mapperclient.HttpMethodAll},
 				LastSeen:             timestamp,
+				Methods:              []mapperclient.HttpMethod{},
 			}
+
+			method, ok := HTTPMethodsToGQLMethods[connWithPath.RequestMethod]
+			if ok {
+				istioConnection.Methods = []mapperclient.HttpMethod{method}
+			}
+
+			connectionPairToGraphQLConnection[connectionPair] = istioConnection
 			continue
 		}
+
 		if timestamp.After(istioConnection.LastSeen) {
 			istioConnection.LastSeen = timestamp
 		}
 
-		connectionPairToConn[connectionPair] = istioConnection
+		method, ok := HTTPMethodsToGQLMethods[connWithPath.RequestMethod]
+		if ok {
+			istioConnection.Methods = append(istioConnection.Methods, method)
+		}
+
+		connectionPairToGraphQLConnection[connectionPair] = istioConnection
 	}
 
-	return lo.Values(connectionPairToConn)
+	return lo.Values(connectionPairToGraphQLConnection)
 }
