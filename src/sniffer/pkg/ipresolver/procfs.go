560a34d23f869320e064912df8c46cfc94e396e2
Ro'e Katz <roekatz@gmail.com>
2023-07-07 18:52:44 +0300

    fixup! Introduce IP resolver by procfs monitoring

--- src/sniffer/pkg/ipresolver/procfs.go
+++ src/sniffer/pkg/ipresolver/procfs.go
@@ -168,12 +168,12 @@ func (r *ProcFSIPResolver) onProcessNew(pid int64, pDir string) {
 
 	if entry, ok := r.byAddr[ipaddr]; ok {
 		if entry.Hostname == hostname {
-			// Already mapped to this hostname, add another process referece
+			// Already mapped to this hostname, add another process reference
 			r.byPid[pid] = entry
 			entry.ProcessRefCount++
 			return
 		} else {
-			// Shouldn't happen
+			// Shouldn't happen - it could happen if an ip replaces its pod very fast and the current single scan sees the new process and not the older one
 			logrus.Warnf("IP mapping conflict: %s got new hostname %s, but already mapped to %s. Would use the newer hostname", ipaddr, hostname, entry.Hostname)
 			// For now, treat it as a new IP mapping (make sure at exit to decrement ref count only if hostname matches)
 		}
