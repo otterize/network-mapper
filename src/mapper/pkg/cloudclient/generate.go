89f9f12d94f587433a1e508fd0f5df14fa06d981
Ori Shoshan <ori@otterize.com>
2023-08-25 09:33:28 +0200

    Don't redownload gql schema from cloud in CI

--- src/mapper/pkg/cloudclient/generate.go
+++ src/mapper/pkg/cloudclient/generate.go
@@ -2,6 +2,7 @@ package cloudclient
 
 import _ "github.com/suessflorian/gqlfetch"
 
-//go:generate sh -c "go run github.com/suessflorian/gqlfetch/gqlfetch --endpoint https://app.staging.otterize.com/api/graphql/v1beta > schema.graphql"
+// The check for $CI makes sure we don't redownload the schema in CI.
+//go:generate sh -c "if [ -z $CI ]; then go run github.com/suessflorian/gqlfetch/gqlfetch --endpoint https://app.staging.otterize.com/api/graphql/v1beta > schema.graphql; fi"
 //go:generate go run github.com/Khan/genqlient ./genqlient.yaml
 //go:generate go run github.com/golang/mock/mockgen@v1.6.0 -destination=./mocks/mocks.go  -package=cloudclientmocks -source=./cloud_client.go CloudClient
