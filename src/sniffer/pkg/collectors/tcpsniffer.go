f3c45bceb6c9ddeb67b610b8274bfe4834b1f4d9
Netanel Bollag <netanel.b@otterize.com>
2024-03-28 09:44:25 +0100

    Handle parsing error

--- src/sniffer/pkg/collectors/tcpsniffer.go
+++ src/sniffer/pkg/collectors/tcpsniffer.go
@@ -74,7 +74,12 @@ func (s *TCPSniffer) HandlePacket(packet gopacket.Packet) {
 	captureTime := detectCaptureTime(packet)
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
 	if ipLayer != nil {
-		ip, _ := ipLayer.(*layers.IPv4)
+		ip, ok := ipLayer.(*layers.IPv4)
+		if !ok {
+			logrus.Debugf("Failed to parse IP layer")
+			return
+		}
+
 		srcIP := ip.SrcIP.String()
 		dstIP := ip.DstIP.String()
 		localHostname, err := s.resolver.ResolveIP(srcIP)
