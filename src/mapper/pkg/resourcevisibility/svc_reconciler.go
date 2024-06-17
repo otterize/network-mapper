e06a975f9bdec32449a19fbdbcadd79f8af0318a
Netanel Bollag <54136032+NetanelBollag@users.noreply.github.com>
2024-06-17 15:13:59 +0200

    Add cache to services reporting (#222)

--- src/mapper/pkg/resourcevisibility/svc_reconciler.go
+++ src/mapper/pkg/resourcevisibility/svc_reconciler.go
@@ -1,36 +1,53 @@
 package resourcevisibility
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"github.com/hashicorp/golang-lru/v2/expirable"
 	"github.com/otterize/intents-operator/src/shared/errors"
 	"github.com/otterize/intents-operator/src/shared/injectablerecorder"
 	"github.com/otterize/network-mapper/src/mapper/pkg/cloudclient"
+	"github.com/otterize/network-mapper/src/mapper/pkg/config"
 	"github.com/otterize/network-mapper/src/mapper/pkg/graph/model"
 	"github.com/samber/lo"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"hash/crc32"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"slices"
 	"time"
 )
 
 type ServiceReconciler struct {
 	client.Client
 	injectablerecorder.InjectableRecorder
-	otterizeCloud cloudclient.CloudClient
-	kubeFinder    KubeFinder
+	otterizeCloud                    cloudclient.CloudClient
+	kubeFinder                       KubeFinder
+	namespaceToReportedServicesCache *expirable.LRU[string, []byte]
 }
 
 type KubeFinder interface {
 	ResolveOtterizeIdentityForService(ctx context.Context, service *corev1.Service, now time.Time) (model.OtterizeServiceIdentity, bool, error)
 }
 
+func OnEvict(key string, _ []byte) {
+	logrus.WithField("namespace", key).Debug("key evicted from cache, you may change configuration to increase cache size or TTL")
+}
+
 func NewServiceReconciler(client client.Client, otterizeCloudClient cloudclient.CloudClient, kubeFinder KubeFinder) *ServiceReconciler {
+	size := viper.GetInt(config.ServiceCacheSizeKey)
+	ttl := viper.GetDuration(config.ServiceCacheTTLDurationKey)
+	cache := expirable.NewLRU[string, []byte](size, OnEvict, ttl)
 	return &ServiceReconciler{
-		Client:        client,
-		otterizeCloud: otterizeCloudClient,
-		kubeFinder:    kubeFinder,
+		Client:                           client,
+		otterizeCloud:                    otterizeCloudClient,
+		kubeFinder:                       kubeFinder,
+		namespaceToReportedServicesCache: cache,
 	}
 }
 
@@ -63,12 +80,47 @@ func (r *ServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		return ctrl.Result{}, errors.Wrap(err)
 	}
 
+	hashSum, err := r.getCachedValue(servicesToReport)
+	if err != nil {
+		return ctrl.Result{}, errors.Wrap(err)
+	}
+
+	val, found := r.namespaceToReportedServicesCache.Get(namespace)
+	if found && bytes.Equal(val, hashSum) {
+		logrus.WithField("namespace", namespace).Debug("Skipping reporting of services in namespace due to cache")
+		return ctrl.Result{}, nil
+	}
+
 	err = r.otterizeCloud.ReportK8sServices(ctx, namespace, servicesToReport)
 	if err != nil {
 		return ctrl.Result{}, errors.Wrap(err)
 	}
 
+	r.namespaceToReportedServicesCache.Add(namespace, hashSum)
+
 	return ctrl.Result{}, nil
+}
+
+func (r *ServiceReconciler) getCachedValue(servicesToReport []cloudclient.K8sServiceInput) ([]byte, error) {
+	values := lo.Map(servicesToReport, func(service cloudclient.K8sServiceInput, _ int) string {
+		return reportedServicesCacheValuePart(service)
+	})
+
+	slices.Sort(values)
+
+	hash := crc32.NewIEEE()
+	for _, value := range values {
+		_, err := hash.Write([]byte(value))
+		if err != nil {
+			return nil, errors.Wrap(err)
+		}
+	}
+	hashSum := hash.Sum(nil)
+	return hashSum, nil
+}
+
+func reportedServicesCacheValuePart(service cloudclient.K8sServiceInput) string {
+	return fmt.Sprintf("%s-%s-%s", service.ResourceName, service.OtterizeServer, service.Service.Spec.Type.Item)
 }
 
 func (r *ServiceReconciler) convertToCloudServices(ctx context.Context, services []corev1.Service) ([]cloudclient.K8sServiceInput, error) {
