024619ca0bb437e87127cb7f49ae2fbae697ae06
Ori Shavit <ori.shavit@otterize.com>
2024-08-22 18:57:48 +0200

    pass arch flag

--- src/ebpf/openssl/openssl.ebpf.c
+++ src/ebpf/openssl/openssl.ebpf.c
@@ -1,6 +1,13 @@
 //go:build ignore
-#include "vmlinux_aarch64.h"
+
+#ifdef TARGET_ARCH_amd64
 #include "vmlinux_x86_64.h"
+#endif
+
+#ifdef TARGET_ARCH_arm64
+#include "vmlinux_aarch64.h"
+#endif
+
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
