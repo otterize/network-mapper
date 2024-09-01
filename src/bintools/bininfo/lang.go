384a9128d3dc3a5acefd5e5baeba1f3e38c4d85d
davidrobert <david.r@otterize.com>
2024-09-01 11:36:02 +0300

    Added nodejs support

--- src/bintools/bininfo/lang.go
+++ src/bintools/bininfo/lang.go
@@ -12,6 +12,7 @@ type SourceLanguage string
 const (
 	GoLang SourceLanguage = "golang"
 	Python SourceLanguage = "python"
+	NodeJs SourceLanguage = "nodejs"
 	Binary SourceLanguage = "binary"
 )
 
@@ -22,6 +23,9 @@ func GetSourceLanguage(path string, f *os.File) SourceLanguage {
 	if isPythonCommand(path) {
 		return Python
 
+	}
+	if isNodeCommand(path) {
+		return NodeJs
 	}
 
 	return Binary
@@ -42,4 +46,16 @@ func isPythonCommand(path string) bool {
 	// Check if the actual path is a python binary - path will be something along /usr/bin/python3
 	_, file := filepath.Split(actualPath)
 	return strings.Contains(file, "python")
+}
+
+func isNodeCommand(path string) bool {
+	// Resolve the symlink to the actual executable path
+	actualPath, err := os.Readlink(path)
+	if err != nil {
+		return false
+	}
+
+	// Check if the actual path is a python binary - path will be something along /usr/bin/node
+	_, file := filepath.Split(actualPath)
+	return strings.Contains(file, "node")
 }
